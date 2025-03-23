6c87a751e5466a03f756ccbe5d6a2f68aa277cb1
ashu <nayaksubhashish456@gmail.com>
2025-03-23 20:39:32 +0530

    Updated App structure, removed Portfolio component, and added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Portfolio from './Portfolio'
+// App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Remove BrowserRouter import
+import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
+import SongList from "./components/SongList";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Portfolio/>
-    </div>
-  )
-}
+    <>
+      {/* Navbar is displayed on all pages */}
+      <Navbar />
 
-export default App
+      {/* Define routes for Portfolio and SongList */}
+      <Routes>
+        <Route path="/" element={<Portfolio />} /> {/* Home page */}
+        <Route path="/music" element={<SongList />} /> {/* Music page */}
+      </Routes>
+    </>
+  );
+};
+
+export default App;
