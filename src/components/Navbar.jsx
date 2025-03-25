c1b4e5fa10af3ada295809e3581d61b52ba2d1b2
ashu <nayaksubhashish456@gmail.com>
2025-03-25 14:37:39 +0530

    Updated Navbar component and added CV file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       />
 
       {/* Download Resume Link */}
-      <NavItem icon={<DownloadIcon />} tooltip="Resume" link="/Resume-jan-25.pdf" download="Subhashish_Nayak_Resume.pdf" />
+      <NavItem icon={<DownloadIcon />} tooltip="Resume" link="/Cv.pdf" download="Subhashish_Nayak_Resume.pdf" />
     </nav>
   );
 };
